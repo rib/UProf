0152c61e17404b5f2cf0a6a69029b88b310dee0b
Robert Bragg <bob@o-hand.com>
2009-03-17 23:49:55 +0000

    [tests/simple] Removes an out of date TODO comment

--- tests/simple.c
+++ tests/simple.c
@@ -27,31 +27,6 @@ main (int argc, char **argv)
 
   context = uprof_context_new ("Simple context");
 
-  /* TODO
-   * I think the details about how timers/counters are declared probably needs
-   * more thought. The static declaration requirements can possibly be reduced
-   * to an enum of timer names, and the name can instead be implicitly
-   * associated with the timer the first time it is used: e.g.
-   *
-   * typedef enum _MyTimers {
-   *  MY_TIMER_A,
-   *  MY_TIMER_B
-   * } MyTimers;
-   *
-   * #define UPROF_START_TIMER(TIMER, PARENT, DESCRIPTION) ...
-   *
-   * The internal UProfTimer structure can then have a bitfield to determine
-   * if this is the first time we have seen the timer; if yes:
-   *  - use the TIMER enum to index into the internal array of timer structures
-   *  - associated the __FILE__, __LINE__, __FUNCTION__ and DESCRIPTION with
-   *    the internal timer structure.
-   *  - save the current rdts value.
-   * else:
-   *  - use the TIMER enum to index into the internal array of timer structures
-   *  - save the current rdts value.
-   *
-   */
-
   delay.tv_sec = 0;
   delay.tv_nsec = 1000000000/4;
 
