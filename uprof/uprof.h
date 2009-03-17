98a64c431b7342b09b47ad7a6b43bf459d2c8f7e
Robert Bragg <bob@o-hand.com>
2009-03-17 23:08:29 +0000

    Updates copyright notices

--- uprof/uprof.h
+++ uprof/uprof.h
@@ -1,3 +1,22 @@
+/*
+ * This file is part of UProf.
+ *
+ * Copyright © 2008, 2009 Robert Bragg
+ *
+ * UProf is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * UProf is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with UProf.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef _UPROF_H_
 #define _UPROF_H_
 
