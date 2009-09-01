cb5067dbd9efcd35d752ece5077e9cf52afe61d2
Robert Bragg <robert@linux.intel.com>
2009-09-01 20:02:53 +0100

    Adds new sanity check test

---
+++ tests/sanity_check.c
@@ -0,0 +1,53 @@
+
+#include <uprof.h>
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdint.h>
+
+UPROF_STATIC_TIMER (full_timer,
+                    NULL, /* no parent */
+                    "Full timer",
+                    "A timer for the test delays in loop0",
+                    0 /* no application private data */
+);
+
+
+int
+main (int argc, char **argv)
+{
+  UProfContext *context;
+  uint64_t prev_counter;
+  uint64_t counter;
+  int i;
+
+  uprof_init (&argc, &argv);
+
+  context = uprof_context_new ("Simple context");
+
+  prev_counter = uprof_get_system_counter ();
+  for (i = 0; i < 1000; i++)
+    {
+      uint64_t diff;
+      counter = uprof_get_system_counter ();
+      diff = counter - prev_counter;
+      prev_counter = counter;
+      g_print ("diff = %llu\n", (unsigned long long)diff);
+    }
+
+  g_print ("<start Full timer>\n");
+  g_print ("  <delay: 1 sec>\n");
+
+  UPROF_TIMER_START (context, full_timer);
+  sleep (1);
+  UPROF_TIMER_STOP (context, full_timer);
+  g_print ("<stop Full timer>\n");
+
+  g_print ("Full timer should have a total duration of 1 second:\n");
+  uprof_context_output_report (context);
+
+  uprof_context_unref (context);
+
+  return 0;
+}
+
